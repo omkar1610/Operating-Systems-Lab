f043754496e9887905e7a2570b837572b371131e
Sarthak Chakraborty <sarthak.chakraborty@gmail.com>
2019-02-01 12:07:12 +0530

    Preemptive SJF not done

--- Assignment 3/a3_14.cpp
+++ Assignment 3/a3_14.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 #include<string>
+#include<random>
 #include<unistd.h>
 #define MEAN 1.0
 using namespace std;
@@ -92,7 +93,49 @@ int npsjf(Process p[], int N){
 
 
 int psjf(Process p[], int N){
-	int tn = 0;
+	int i, j, k, tn = 0, t = 0;
+	queue <Process> q;
+	Process a;
+	vector <Process> vec;
+	vector <Process>::iterator it;
+
+	i=0;
+	j=0;
+	while(1){
+		if(i >= N)
+			break;
+		for(k=j; k<N; k++){
+			if(t >= p[k].arr_time)
+				q.push(p[k]);
+			else
+				break;
+		}
+		j=k;
+		while(!q.empty()){
+			vec.push_back(q.front());
+			q.pop();
+		}
+		sort(vec.begin(), vec.end(), compare);
+		for(it=vec.begin(); it<vec.end(); it++){
+			q.push(*it);
+		}
+		if(!q.empty()){
+			a = q.front();
+			q.pop();
+			if(a.cpu_burst == 1){
+				tn += (t-a.arr_time) + 1;
+				i++;
+			}
+			else{
+				a.cpu_burst -= 1;
+				tn += (t-a.arr_time) + 1;
+				a.arr_time += 1;
+				q.push(a);
+			}
+		}
+		t++;
+	}
+
 	return tn;
 }
 
@@ -154,6 +197,11 @@ int hrn(Process p[], int N){
 
 int main(int argc, char *argv[]){
 	int N, t = 0, i, tn_fcfs, tn_npsjf, tn_psjf, tn_rr, tn_hrn;
+	random_device rdb, rda;
+	mt19937 gen1(rdb());
+	mt19937 gen2(rda());
+  	std::uniform_int_distribution<> unidist(1,20);
+  	std::uniform_real_distribution<> unidist_d(0.0,1.0);
 	double r;
 	N = atoi(argv[1]);
 
@@ -162,9 +210,9 @@ int main(int argc, char *argv[]){
 	for(i=0; i<N; i++){
 		p[i].pid = i;
 		p[i].arr_time = t;
-		p[i].cpu_burst = 1 + rand()%20;
-		r = ((double)rand() / RAND_MAX);
-		t += (int)((-1.0 / MEAN)*log(1-r)) % 10;
+		p[i].cpu_burst = unidist(gen1);
+		r = unidist_d(gen2);
+		t += (int)((-1.0 / MEAN)*log(1-r));
 	}
 
 	ofstream f ("process.txt");
--- Assignment 3/script.py
+++ Assignment 3/script.py
@@ -6,14 +6,23 @@ from subprocess import Popen, PIPE
 print("Python script to run a3_14.cpp file")
 
 cmd = "a3_14.cpp"
-subprocess.call(["g++",cmd])
+subprocess.call(["g++","-std=c++11",cmd])
 
-args = ["10", "50", "100"]
+[[[1,2,3,4],[10,20,30,40]],
+ [[1,2,3,4],[10,20,30,40]]]
+
+# args = ["10", "50", "100"]
+args = ["4"]
 tn = []
 tn = np.array(tn)
+atn_fcfs = atn_npsjf = atn_psjf = atn_rr = atn_hrn = [[],[]]
+atn = [[[],[]]]
 for i in range(len(args)):
 	tn_fcfs = tn_npsjf = tn_psjf = tn_rr = tn_hrn = 0.0
-	for j in range(10):
+	ttn = []
+	ttn = np.array(ttn)
+	np.zeros(ttn)
+	for j in range(1):
 		result = subprocess.Popen(["./a.out",args[i]], stdout=subprocess.PIPE)
 		out = result.stdout.read()
 		tn = out.split('\n')[:-1]
@@ -23,11 +32,25 @@ for i in range(len(args)):
 		tn_psjf += tn[2]
 		tn_rr += tn[3]
 		tn_hrn += tn[4]
-		# print(tn)
+		print(tn)
 	
-	print("FCFS ATN: " + str(float(tn_fcfs)/10))
-	print("NPSJF ATN: " + str(float(tn_npsjf)/10))
-	print("PSJF ATN: " + str(float(tn_psjf)/10))
-	print("RR ATN: " + str(float(tn_rr)/10))
-	print("HRN ATN: " + str(float(tn_hrn)/10))
-	print("\n")
+	atn_fcfs[0].append(int(args[i]))
+	atn_fcfs[1].append(float(tn_fcfs)/10)
+
+	atn_npsjf[0].append(int(args[i]))
+	atn_npsjf[1].append(float(tn_npsjf)/10)
+
+	atn_psjf[0].append(int(args[i]))
+	atn_psjf[1].append(float(tn_psjf)/10)
+
+	atn_rr[0].append(int(args[i]))
+	atn_rr[1].append(float(tn_rr)/10)
+
+	atn_hrn[0].append(int(args[i]))
+	atn_hrn[1].append(float(tn_hrn)/10)
+
+
+plt.figure()
+plt.plot(atn_fcfs[0], atn_fcfs[1])
+plt.plot(atn_npsjf[0], atn_npsjf[1], 'g')
+plt.show()
