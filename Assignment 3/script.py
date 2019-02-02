9332e2c18378bdf40c225df631bbf5206ffd11c6
Sarthak Chakraborty <sarthak.chakraborty@gmail.com>
2019-02-02 17:55:15 +0530

    Plots done

--- Assignment 3/script.py
+++ Assignment 3/script.py
@@ -9,14 +9,14 @@ cmd = "a3_14.cpp"
 subprocess.call(["g++","-std=c++11",cmd])
 
 color = ["r","g","b","y","k"]
-args = ["10", "20", "30", "40", "50", "60", "70", "80", "90", "100"]
+args = ["10", "20", "30", "40", "50", "60", "70", "80", "90", "100"]#, "120", "140", "160", "180", "200"]
+labels = ["FCFS", "Non-Preemptive SJF", "Preemptive SJF", "Round Robin", "HRN"]
 # args = ["10", "50", "100"]
 tn = []
 atn = [[0.0 for i in range(len(args))] for j in range(5)]
 
 for i in range(len(args)):
 	for j in range(10):
-		print(i)
 		result = subprocess.Popen(["./a.out", args[i]], stdout=subprocess.PIPE)
 		out = result.stdout.read()
 		tn = out.split('\n')[:-1]
@@ -26,10 +26,13 @@ for i in range(len(args)):
 	for k in range(5):
 		atn[k][i] /= 10.0
 
-
+plt.figure()
 args = np.array(map(int, args))
 for k in range(5):
-	plt.figure()
-	plt.plot(args, atn[k], c=color[k])
+	# plt.figure()
+	plt.scatter(args, atn[k], c=color[k], s=5)
+	plt.xticks(args)
+	plt.legend(labels)
+	plt.savefig("Fig. "+str(k)+".png")
 
-plt.show()
+# plt.show()
