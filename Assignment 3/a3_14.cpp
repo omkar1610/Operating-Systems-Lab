cdcfd7fddca0bbea95640a3977d0584751625271
Sarthak Chakraborty <sarthak.chakraborty@gmail.com>
2019-02-04 00:10:58 +0530

    Add files via upload

--- Assignment 3/a3_14.cpp
+++ Assignment 3/a3_14.cpp
@@ -5,6 +5,7 @@
 #define MEAN 1.0
 using namespace std;
 
+// Structure defined for a Process, which contains Process no., arrival time and CPU burst
 typedef struct{
 	int pid;
 	int arr_time;
@@ -12,6 +13,7 @@ typedef struct{
 }Process;
 
 
+// First Come First Serve
 int fcfs(Process p[], int N){
 	int i, j, k, tn = 0, t = 0;
 	Process a;
@@ -19,7 +21,9 @@ int fcfs(Process p[], int N){
 
 	i=0;
 	j=0;
+	// i denotes the number of processes completed
 	while(i < N){
+		// Push the processes that has arrived uptil now in Job Queue
 		for(k=j; k<N; k++){
 			if(t >= p[k].arr_time)
 				q.push(p[k]);
@@ -27,6 +31,7 @@ int fcfs(Process p[], int N){
 				break;
 		}
 		j=k;
+		// If the queue in not empty, them take the first process and change the time and turnaround time accordingly
 		if(!q.empty()){
 			a = q.front();
 			q.pop();
@@ -34,6 +39,7 @@ int fcfs(Process p[], int N){
 			tn += (t-a.arr_time) + a.cpu_burst; 
 			t += a.cpu_burst;
 		}
+		// If the queue is empty but all the processes have not been completed, increase time
 		else if(i < N){
 			t++;
 			continue;
@@ -41,12 +47,16 @@ int fcfs(Process p[], int N){
 		else
 			break;
 	}
+	// Return Average Turnaround Time
 	return tn/N;
 }
 
+
+// Function to compare the CPU burst of two processes
 bool compareCPU(Process a, Process b){
 	return a.cpu_burst<b.cpu_burst;
 }
+// Non Preemptive Shortest Job First
 int npsjf(Process p[], int N){
 	queue <Process> q;
 	vector <Process> vec;
@@ -56,8 +66,11 @@ int npsjf(Process p[], int N){
 
 	i=0;
 	j=0;
+	// i denotes the number of processes that has been completed till now
 	while(i < N){
+		// Clear the vector
 		vec.clear();
+		// Push the processes in the job queue
 		for(k=j; k<N; k++){
 			if(t >= p[k].arr_time){
 				q.push(p[k]);
@@ -66,6 +79,7 @@ int npsjf(Process p[], int N){
 				break;
 		}
 		j=k;
+		// Put all the jobs in the queue in vector, sort it and put it back in the queue
 		while(!q.empty()){
 			vec.push_back(q.front());
 			q.pop();
@@ -74,6 +88,7 @@ int npsjf(Process p[], int N){
 		for(it=vec.begin(); it<vec.end(); it++){
 			q.push(*it);
 		}
+		// If queue is not empty, take the first job in the queue (now the shortest one) and complete the process changing the turnaround time accordingly
 		if(!q.empty()){
 			a = q.front();
 			q.pop();
@@ -81,6 +96,7 @@ int npsjf(Process p[], int N){
 			tn += (t-a.arr_time) + a.cpu_burst; 
 			t += a.cpu_burst;
 		}
+		// If the queue is empty, and all the processes has not been completed, then increase the time by 1
 		else if(i < N){
 			t++;
 			continue;
@@ -88,10 +104,12 @@ int npsjf(Process p[], int N){
 		else
 			break;
 	}
+	// Return average time burst
 	return tn/N;
 }
 
 
+// Preemptive Shortest Job First
 int psjf(Process p[], int N){
 	int i, j, k, tn = 0, t = 0;
 	queue <Process> q;
@@ -101,10 +119,13 @@ int psjf(Process p[], int N){
 
 	i=0;
 	j=0;
+	// while time increases
 	while(1){
 		vec.clear();
+		// Break if all the processes has been completed
 		if(i >= N)
 			break;
+		// Push the processes in the job queue
 		for(k=j; k<N; k++){
 			if(t >= p[k].arr_time)
 				q.push(p[k]);
@@ -112,6 +133,7 @@ int psjf(Process p[], int N){
 				break;
 		}
 		j=k;
+		// Sort the processes by their CPU burst
 		while(!q.empty()){
 			vec.push_back(q.front());
 			q.pop();
@@ -123,11 +145,13 @@ int psjf(Process p[], int N){
 		if(!q.empty()){
 			a = q.front();
 			q.pop();
+			// If the CPU burst left is 1, then increase i by 1 and turnaorund time accordingly
 			if(a.cpu_burst == 1){
 				tn += (t-a.arr_time) + 1;
 				t++;
 				i++;
 			}
+			// If the CPU burst left is more than 1, then decrease the burst by 1, and push the process again in Queue
 			else{
 				a.cpu_burst -= 1;
 				tn += (t-a.arr_time) + 1;
@@ -141,13 +165,15 @@ int psjf(Process p[], int N){
 }
 
 
+// Round Robin
 int rr(Process p[], int N){
 	queue <Process> q;
 	Process a;
-	int i, j, k, tn = 0, t = 0, delta = 2;
+	int i, j, k, tn = 0, t = 0, delta = 2; // Time quantum = 2
 
 	i=0;
 	j=0;
+	// While all the process has not been completed
 	while(i < N){
 		for(k=j; k<N; k++){
 			if(t >= p[k].arr_time)
@@ -159,6 +185,7 @@ int rr(Process p[], int N){
 		if(!q.empty()){
 			a = q.front();
 			q.pop();
+			// If busrt > time quantum, then decrease the burst by delta and push the process in Queue after pushiing all the processes that has come in between its start and forced stop
 			if(a.cpu_burst>delta){
 				a.cpu_burst -= delta;
 				tn += (t-a.arr_time) + delta;
@@ -173,6 +200,7 @@ int rr(Process p[], int N){
 				j=k;
 				q.push(a);
 			}
+			// If burst <= delta, then 1 process is completed
 			else{
 				i++;
 				tn += (t-a.arr_time) + a.cpu_burst;
@@ -190,11 +218,14 @@ int rr(Process p[], int N){
 }
 
 
+
+// Compares the Respose Ratio of two processes
 bool comparePriority(const pair<Process, int> &a, const pair<Process, int> &b){
 	double rRatio1 = ((double)(a.second - a.first.arr_time) + a.first.cpu_burst) / a.first.cpu_burst;
 	double rRatio2 = ((double)(b.second - b.first.arr_time) + b.first.cpu_burst) / b.first.cpu_burst;
 	return rRatio1>rRatio2;
 }
+// Highest Response Ratio Next
 int hrn(Process p[], int N){
 	queue <Process> q;
 	vector < pair<Process, int> > vec;
@@ -213,6 +244,7 @@ int hrn(Process p[], int N){
 				break;
 		}
 		j=k;
+		// Sort all the jobs according to their priority
 		while(!q.empty()){
 			vec.push_back(make_pair(q.front(), t));
 			q.pop();
@@ -221,6 +253,7 @@ int hrn(Process p[], int N){
 		for(it=0; it<vec.size(); it++){
 			q.push(vec[it].first);
 		}
+		// If the queue is not empty, take the first job (highest priority) and complete it
 		if(!q.empty()){
 			a = q.front();
 			q.pop();
@@ -244,13 +277,15 @@ int main(int argc, char *argv[]){
 	random_device rdb, rda;
 	mt19937 gen1(rdb());
 	mt19937 gen2(rda());
-  	std::uniform_int_distribution<> unidist(1,20);
-  	std::uniform_real_distribution<> unidist_d(0.0,1.0);
+  	std::uniform_int_distribution<> unidist(1,20);	// Generates an integer uniformly between 1 and 20
+  	std::uniform_real_distribution<> unidist_d(0.0,1.0);	// Generates a real number uniformly between 1 and 1
 	double r;
+	// Taking input from the command line argument
 	N = atoi(argv[1]);
 
-	srand(getpid());
+	srand(getpid()); // Seed the random numbbers with pid
 	Process p[N];
+	// Generates the arrival time and CPU burst for the N processes
 	for(i=0; i<N; i++){
 		p[i].pid = i;
 		p[i].arr_time = t;
@@ -259,7 +294,9 @@ int main(int argc, char *argv[]){
 		t += (int)((-1.0 / MEAN)*log(1-r));
 	}
 
-	ofstream f ("process.txt");
+	// Saves the data in a file
+	string file = "Process_"+to_string(N)+"_"+to_string(getpid())+".txt";
+	ofstream f (file);
 	if(f.is_open()){
 		f<<"PROCESS ID\tARRIVAL TIME\tCPU BURST"<<endl;
 		for(i=0; i<N; i++){
--- Assignment 3/script.py
+++ Assignment 3/script.py
@@ -9,14 +9,14 @@ cmd = "a3_14.cpp"
 subprocess.call(["g++","-std=c++11",cmd])
 
 color = ["r","g","b","y","k"]
-# args = ["10", "20", "30", "40", "50", "60", "70", "80", "90", "100"]#, "120", "140", "160", "180", "200"]
+args = ["10", "20", "30", "40", "50", "60", "70", "80", "90", "100", "120", "140", "160", "180", "200"]
 labels = ["FCFS", "Non-Preemptive SJF", "Preemptive SJF", "Round Robin", "HRN"]
-args = ["10", "50", "100"]
+# args = ["10", "50", "100"]
 tn = []
 atn = [[0.0 for i in range(len(args))] for j in range(5)]
 
 for i in range(len(args)):
-	for j in range(10):
+	for j in range(1):
 		print(i)
 		result = subprocess.Popen(["./a.out", args[i]], stdout=subprocess.PIPE)
 		out = result.stdout.read()
@@ -25,18 +25,20 @@ for i in range(len(args)):
 		for k in range(5):
 			atn[k][i] += tn[k]
 	for k in range(5):
-		atn[k][i] /= 10.0
+		atn[k][i] /= 1.0
 
-plt.figure()
+# plt.figure()
 args = np.array(map(int, args))
 for k in range(5):
-	# plt.figure()
-	plt.scatter(args, atn[k], c=color[k])
+	plt.figure()
+	plt.plot(args, atn[k], c=color[k])
 	plt.xlabel("Number of Jobs (N)")
 	plt.ylabel("Average Turnaround Time")
 	plt.xticks(args)
-	# plt.title(labels[k])
-	plt.legend(labels)
+	plt.title(labels[k])
+	# plt.legend(labels)
 	plt.savefig("Fig. "+str(k)+".png")
 
 # plt.show()
+
+
